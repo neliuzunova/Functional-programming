3c575d1f6119bfcd6e63ed748799ac45dabc2eac
neliuzunova <46846738+neliuzunova@users.noreply.github.com>
2019-10-21 15:56:47 +0300

    Add files via upload

--- upr3.rkt
+++ upr3.rkt
@@ -29,3 +29,32 @@
 (define (my-sin m x)
   (sum 0 m (lambda (n) (* (expt (-1) n)(/ (expt x (+ 1 (* 2 n))) (fact (+ 1 (* 2 n)))))) 1+)
   )
+
+(define (my-scos m x)
+  (sum 0 m (lambda (n) (* (expt (-1) n)(/ (expt x (* 2 n)) (fact (* 2 n))))) 1+)
+  )
+
+(define (product a b term next) 
+  (if (> a b)
+        1
+        (* (term a) (product (next a) b term next)))
+  )
+
+
+(define (accumulate op nv a b term next)
+  (if (> a b)
+      nv
+      (op (term a) (accumulate op nv (next a) b term next)))
+  )
+
+
+(define (fact2 n)
+  (accumulate * 1 1 n (lambda (x) x) 1+))
+
+(define (sum2 a b term next)
+  (accumulate + 0 a b term next))
+
+(define (product2 a b term next)
+  (accumulate * 1 a b term next))
+
+
